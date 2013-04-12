d326a48c3a395d310d6257a42294f857ae7fa6bc
Athlan <athlan@vgroup.pl>
2013-04-12 19:09:13 +0200

    init commit

---
+++ module/Application/config/module.config.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Application;
+
+return array(
+    'console' => array(
+        'router' => array(
+            'routes' => require __DIR__ . '/router.config.php',
+        )
+    ),
+    
+    'controllers' => array(
+        'invokables' => array(
+            'Controller\Index' => 'Application\Controller\IndexController'
+        ),
+    ),
+    
+    'doctrine' => array(
+        'driver' => array(
+            __NAMESPACE__ . '_driver' => array(
+                'class' => 'Doctrine\ORM\Mapping\Driver\AnnotationDriver',
+                'cache' => 'array',
+                'paths' => array(__DIR__ . '/../src/' . __NAMESPACE__ . '/Entity'),
+            ),
+            'orm_default' => array(
+                'drivers' => array(
+                    __NAMESPACE__ . '\Entity' => __NAMESPACE__ . '_driver'
+                )
+            )
+        )
+    ),
+    
+);
---
+++ module/Application/src/Application/Controller/Util/BaseCliController.php
@@ -0,0 +1,32 @@
+<?php
+namespace Application\Controller\Util;
+
+use Zend\Mvc\MvcEvent;
+
+use Application\Acl\AclAccess;
+
+use Zend\Mvc\Controller\AbstractActionController;
+
+/**
+ * 
+ * @author Athlan
+ *
+ */
+abstract class BaseCliController extends AbstractActionController
+{
+    public function onDispatch(MvcEvent $e) {
+        $request = $this->getRequest();
+
+        // Make sure that we are running in a console and the user has not tricked our
+        // application into running this action from a public web server.
+        if (!$request instanceof \Zend\Console\Request){
+            throw new \RuntimeException('You can only use this action from a console!');
+        }
+        
+        parent::onDispatch($e);
+    }
+    
+    public function getConfig() {
+        return $this->getServiceLocator()->get('Config');
+    }
+}
